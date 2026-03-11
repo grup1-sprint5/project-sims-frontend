286a628737f8c41a202bdbb19f03be9a0e9c533a
sergiobiol <sergiobiol>
2026-03-11 17:33:20 +0100

    fix: resolve merge conflict in main.ts with develop

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import router from './router'
 import Vue3Toastify from 'vue3-toastify'
 import 'vue3-toastify/dist/index.css'
 
+const app = createApp(App)
 const pinia = createPinia()
 
-createApp(App)
-	.use(pinia)
-	.use(router)
-	.use(Vue3Toastify)
-	.mount('#app')
+app
+  .use(pinia)
+  .use(router)
+  .use(Vue3Toastify)
+  .mount('#app')
