12706b94e5f8d12f48bcde807ea3e9a722d28cb7
sergiobiol <sergiobiol>
2026-04-13 16:30:15 +0200

    afegir widget accessibilitat UserWay :::

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  if (!document.getElementById('userway-widget-script')) {
+    const script = document.createElement('script');
+    script.id = 'userway-widget-script';
+    script.src = 'https://cdn.userway.org/widget.js';
+    script.setAttribute('data-account', 'k5lLyDk8Vk');
+    script.async = true;
+    document.body.appendChild(script);
+  }
+});
+</script>
 
 <template>
   <router-view />
